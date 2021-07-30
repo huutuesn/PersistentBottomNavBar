480112ccca865ce8af6909ec05db0ed9ed872d12
Tue Tran <huutueml@gmail.com>
2021-07-30 11:37:26 +0700

    cal height

--- example/lib/payment.dart
+++ example/lib/payment.dart
@@ -36,96 +36,103 @@ class _PaymentPageState extends State<PaymentPage>
   @override
   Widget build(BuildContext context) {
     print('home decor------xxx ${MediaQuery.of(context).devicePixelRatio}');
-    return Stack(
-      children: [
-        Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.width * 429 / 747,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/bg2.png'),
-              fit: BoxFit.fill,
+    return LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+      print('Heigth ${constraints.maxHeight}');
+      return Stack(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.width * 429 / 747,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/bg2.png'),
+                fit: BoxFit.fill,
+              ),
             ),
           ),
-        ),
-        Positioned(
-          top: kToolbarHeight + MediaQuery.of(context).padding.top,
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              ColorfulTabBar(
-                selectedHeight: 48,
-                unselectedHeight: 48,
-                tabs: [
-                  TabItem(
-                      title: Container(
-                        width: 60,
-                        child: Column(
+          Positioned(
+            top: kToolbarHeight + MediaQuery.of(context).padding.top,
+            height: constraints.maxHeight -
+                (kToolbarHeight +
+                    MediaQuery.of(context).padding.top), // constraints - top
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                ColorfulTabBar(
+                  indicatorHeight: 0.0,
+                  selectedHeight: 48,
+                  unselectedHeight: 48,
+                  tabs: [
+                    TabItem(
+                        title: Container(
+                          width: 60,
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(Icons.home_outlined),
+                                SizedBox(width: 8),
+                                Text('Home')
+                              ]),
+                        ),
+                        color: Colors.red.shade600,
+                        unselectedColor: Colors.blue),
+                    TabItem(
+                        title: Column(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
-                              Icon(Icons.home_outlined),
+                              Icon(Icons.favorite_outline),
                               SizedBox(width: 8),
-                              Text('Home')
+                              Text('Favorite')
                             ]),
-                      ),
-                      color: Colors.red.shade600,
-                      unselectedColor: Colors.blue),
-                  TabItem(
-                      title: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(Icons.favorite_outline),
-                            SizedBox(width: 8),
-                            Text('Favorite')
-                          ]),
-                      color: Colors.red.shade600,
-                      unselectedColor: Colors.blue),
-                  TabItem(
-                      title: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(Icons.search_outlined),
-                            SizedBox(width: 8),
-                            Text('Search')
-                          ]),
-                      color: Colors.red.shade600,
-                      unselectedColor: Colors.blue),
-                  TabItem(
-                      title: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(Icons.settings_outlined),
-                            SizedBox(width: 8),
-                            Text('Settings')
-                          ]),
-                      color: Colors.red.shade600,
-                      unselectedColor: Colors.blue),
-                ],
-                controller: _tabController,
-              ),
-              Expanded(
-                child: Container(
-                  //height: 60,
-                  //width: 300,
-                  // color: Colors.blue,
-                  child: TabBarView(
-                      controller: _tabController,
-                      children: List.generate(
-                          4,
-                          (index) => Container(
-                                // height: 60,
-                                // width: 300,
-                                color: Colors.transparent,
-                              ))),
+                        color: Colors.red.shade600,
+                        unselectedColor: Colors.blue),
+                    TabItem(
+                        title: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.search_outlined),
+                              SizedBox(width: 8),
+                              Text('Search')
+                            ]),
+                        color: Colors.red.shade600,
+                        unselectedColor: Colors.blue),
+                    TabItem(
+                        title: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.settings_outlined),
+                              SizedBox(width: 8),
+                              Text('Settings')
+                            ]),
+                        color: Colors.red.shade600,
+                        unselectedColor: Colors.blue),
+                  ],
+                  controller: _tabController,
                 ),
-              ),
-            ],
+                Expanded(
+                  child: Container(
+                    //height: 60,
+                    //width: 300,
+                    // color: Colors.blue,
+                    child: TabBarView(
+                        controller: _tabController,
+                        children: List.generate(
+                            4,
+                            (index) => Container(
+                                  // height: 60,
+                                  // width: 300,
+                                  color: Colors.white,
+                                ))),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-      ],
-    );
+        ],
+      );
+    });
     /* DefaultTabController(
       length: 3,
       child: Container(
